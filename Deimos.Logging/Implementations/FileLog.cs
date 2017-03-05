a18bfe6846436e5fc5b8673c6049efe4e28658d4
Siegmar <devel@siegmar-buss.de>
2017-03-05 15:37:18 +0100

    Implemented a daily log file rotation.

--- Deimos.Logging/Implementations/FileLog.cs
+++ Deimos.Logging/Implementations/FileLog.cs
@@ -1,18 +1,38 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Deimos.Logging.Implementations
 {
     internal class FileLog : ILog
     {
+        private const int Keep = 7;
+
+        private readonly string _fileName;
+        
         private TextWriter _writer;
+        private DateTime _nextRotation;
 
         public FileLog(string filePath)
         {
-            _writer = new StreamWriter(filePath, true);
+            _fileName = filePath;
+            _writer = new StreamWriter(_fileName, true);
+            _nextRotation = DateTime.UtcNow.AddHours(24);
         }
 
         public void AppendRecord(LogRecord rec)
         {
+            if (DateTime.UtcNow >= _nextRotation)
+            {
+                // Close the current log file.
+                _writer.Close();
+
+                RotateLogFiles();
+                _nextRotation = DateTime.UtcNow.AddHours(24);
+
+                // Open the file again
+                _writer = new StreamWriter(_fileName, true);
+            }
+
             lock (_writer)
             {
                 _writer.Write(rec.TimeStamp.ToString("yyyy-MM-dd-HH:mm:ss.fff "));
@@ -21,6 +41,32 @@ namespace Deimos.Logging.Implementations
                 _writer.WriteLine(rec.Message, rec.Args);
                 _writer.Flush();
             }
+        }
+
+        private void RotateLogFiles()
+        {
+            // Delete the oldest file to keep
+            File.Delete(GenerateFileName(Keep));
+
+            // Rename other backups
+            for (int n = Keep - 1; n > 0; n--)
+            {
+                string oldName = GenerateFileName(n);
+                string newName = GenerateFileName(n + 1);
+                if (File.Exists(oldName))
+                {
+                    File.Move(oldName, newName);
+                }
+            }
+
+            // Backup the current file
+            File.Move(_fileName, GenerateFileName(1));
+        }
+
+
+        private string GenerateFileName(int number)
+        {
+            return _fileName + "." + number;
         }
     }
 }
---
+++ Tests/TestFileLoggerRotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using Deimos.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestFileLoggerRotation
+{
+    class Program
+    {
+        private static ILogger Logger = Log.CreateLogger("Test");
+
+        static void Main(string[] args)
+        {
+            Log.ToFile("dummy.log");
+
+            while (true)
+            {
+                Logger.Info("I am alive.");
+                Thread.Sleep(30000);
+            }
+        }
+    }
+}
