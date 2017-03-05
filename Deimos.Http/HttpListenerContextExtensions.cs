a62f4ce05bab586d27463d7c4c6a20ed5aa4bc02
Siegmar <devel@siegmar-buss.de>
2017-03-05 11:38:29 +0100

    Json() can now specify a HTTP result code other than 200.

--- Deimos.Http/HttpListenerContextExtensions.cs
+++ Deimos.Http/HttpListenerContextExtensions.cs
@@ -10,17 +10,19 @@ namespace Deimos.Http
         /// <summary>
         /// Sends the data object as UTF8 encoded JSON response.
         /// </summary>
-        /// <param name="ctx"></param>
-        /// <param name="data"></param>
+        /// <param name="ctx">The listener context for the response.</param>
+        /// <param name="data">The object to format as JSON.</param>
+        /// <param name="statusCode">The status code to return from the request.</param>
         /// <returns>Always <c>true</c>.</returns>
-        public static bool Json(this HttpListenerContext ctx, object data)
+        public static bool Json(this HttpListenerContext ctx, object data, HttpStatusCode statusCode = HttpStatusCode.OK)
         {
             byte[] jsonData = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data));
 
             ctx.Response.ContentEncoding = Encoding.UTF8;
             ctx.Response.ContentType = "application/json";
-            ctx.Response.StatusCode = (int)HttpStatusCode.OK;
+            ctx.Response.StatusCode = (int)statusCode;
             ctx.Response.OutputStream.Write(jsonData, 0, jsonData.Length);
+            ctx.Response.OutputStream.Flush();
             ctx.Response.OutputStream.Close();
 
             return true;
